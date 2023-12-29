e218105ed1a21a49a72fd6472d3a84aaa0f7636b
Phyo Zaw Hein <phyozawhein1997@gmail.com>
2023-12-29 02:38:56 -0500

    Starting over

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
 
-import './App.scss';
-import Home from './components/home/Home.js'
-import Layout from './components/layout/Layout';
-import Skills from './components/skills/Skills';
-import {Routes, Route} from 'react-router-dom';
 function App() {
   return (
-    <>
-      <Routes >
-        <Route path="/*" element={<Layout/>}>
-          <Route index  element={<Home/>} />
-          <Route exact path="skills" element={<Skills/>}/>
-          {/* <Route exact path="bio" element={<Bio/>}/>
-          <Route exact path="projects" element={<Projects/>}/>
-          <Route exact path="contact" element={<Contact/>}/> */}
-        
-        </Route>
-        
-      </Routes>
-    </>
+    <div className="App">
+
+    </div>
   );
 }
 
